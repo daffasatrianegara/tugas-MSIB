a23fa75722c8539612669bbd3e6efe0e0f519608
daffasatrianegara <daffasatria036@gmail.com>
2024-01-12 20:37:05 +0700

    feat : membuat program JS berdasarkan I/O

---
+++ tes.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync')();
+
+const test = (num) => {
+    let hasil = ''
+    for(let i = 0; i < num; i++) {
+        for(let j = 0; j < num; j++) {
+            if(i % 2 === 0) {
+                j % 2 === 0 ? hasil += '*' : hasil += j
+            } else {
+                j % 2 !== 0 ? hasil += '*' : hasil += j
+            }
+        }
+        hasil += '\n'
+    }
+    const ket = `Output: \n${hasil}`
+    return ket
+}
+
+const hasil = prompt('input: ')
+console.log(test(hasil));
